07ca70109569ffc419d5999df98d9e77e57257ab
Aaron Stockdill <aaron@potatosoftworks.com>
2016-08-23 19:42:44 +1200

    Include a work section that is still (by default) unreachable.

---
+++ html/work/dynamic.php
@@ -0,0 +1,110 @@
+<?php
+if(isset($_COOKIE["style"])) {
+    $theme = $_COOKIE["style"];
+} else {
+    $theme = "white";
+    setcookie("style", "white", time() + 365);
+}
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Aaron Stockdill</title>
+        <?php
+            echo '<link rel="stylesheet" href="/css/'.$theme.'.css" media="screen" id="theme">';
+        ?>
+        <link rel="stylesheet" href="/css/master.css" media="screen">
+        <link rel="stylesheet" href="/css/600.css" media="screen and (max-width: 600px)">
+        <link rel="stylesheet" href="/css/print.css" media="print">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="description" content=""/>
+    </head>
+    <body>
+<div class='container'>
+    <nav>
+        <a href='/'>
+            <span lang="EN">Home</span>
+            <span lang="FR">Accueil</span>
+        </a>
+        <a href='/bio/'>
+            <span lang="EN">About</span>
+            <span lang="FR">Biographie</span>
+        </a>
+        <a href="/work/">
+            <span lang="EN">Works</span>
+            <span lang="FR">Ouvrages</span>
+        </a>
+        <a href='/contact/'>
+            <span lang="EN">Contact Details</span>
+            <span lang="FR">Détails du Contact</span>
+        </a>
+        <a href="/AaronStockdill.pdf" target="_blank">Curriculum Vit&aelig;</a>
+    </nav>
+    <a href='/work/#Writing' class='back-to-works'>
+        <span lang='EN'>Back to Works</span>
+        <span lang='FR'>Retour à Ouvrages</span>
+    </a>
+<div class='writing'>
+<?php
+include_once 'Parsedown.php';
+
+$Parsedown = new Parsedown();
+$rootdir = realpath(dirname(__FILE__));
+$filedir = $rootdir."/../../dynamic/";
+
+if(isset($_GET['id'])) {
+    $filename = $filedir.$_GET['id'].".md";
+    $mdfile = fopen($filename, "r") or die("Unable to open file!");
+    $markdown = fread($mdfile, filesize($filename));
+    fclose($mdfile);
+    echo $Parsedown->text($markdown);
+} else {
+    echo "<h1>404</h1>";
+    echo "File not found.";
+}
+?>
+</div>
+</div>
+<script src='https://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_SVG'></script>
+        <footer>
+            <span class='selector'>
+                <a id="white-button" class='theme-button active' onclick='switch_theme("white")'>
+                    <span lang='EN'>light</span>
+                    <span lang='FR'>blanc</span>
+                </a>
+                <a id="black-button" class='theme-button' onclick='switch_theme("black")'>
+                    <span lang='EN'>dark</span>
+                    <span lang='FR'>noir</span>
+                </a>
+            </span>
+            <!--<span class='separator'></span>
+            <span class='selector'>
+                <a id="EN-button" class='lang-button active' onclick='switch_language("EN")'>
+                    EN
+                </a>
+                <a id="FR-button" class='lang-button' onclick='switch_language("FR")'>
+                    FR
+                </a>
+            </span>-->
+        </footer>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.3/jquery.min.js"></script>
+        <script src="/js/preferences.js" charset="utf-8"></script>
+        <?php
+            if (strpos($_SERVER['SERVER_NAME'],
+                       'aaron.stockdill.nz') !== false) {
+        ?>
+        <script>
+            (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+            })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+            ga('create', 'UA-79422930-1', 'auto');
+            ga('send', 'pageview');
+        </script>
+        <?php
+        }
+        ?>
+    </body>
+</html>
---
+++ html/work/index.php
@@ -0,0 +1,146 @@
+<?php
+if(isset($_COOKIE["style"])) {
+    $theme = $_COOKIE["style"];
+} else {
+    $theme = "white";
+    setcookie("style", "white", time() + 365);
+}
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Work by Aaron Stockdill</title>
+        <?php
+            echo '<link rel="stylesheet" href="/css/'.$theme.'.css" media="screen" id="theme">';
+        ?>
+        <link rel="stylesheet" href="/css/master.css" media="screen">
+        <link rel="stylesheet" href="/css/600.css" media="screen and (max-width: 600px)">
+        <link rel="stylesheet" href="/css/print.css" media="print">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="description" content="A collection of work that I have made.
+"/>
+    </head>
+    <body>
+
+<?php
+date_default_timezone_set('Pacific/Auckland');
+$rootdir = realpath(dirname(__FILE__));
+$filedir = $rootdir."/../../dynamic/";
+define('FILE_DIR', $filedir);
+
+function list_files($type) {
+    $files = scandir(FILE_DIR);
+    $count = 0;
+    foreach ($files as $f) {
+        if ($f[0] == ".") {
+            // pass
+        } else {
+            $parts = explode(".", $f);
+            $title = $parts[0];
+            $date = $parts[1];
+            $extension = $parts[2];
+            if ($extension == $type) {
+                $count += 1;
+                echo "<div class='dynamic-link'>";
+                echo "<a href='/work/dynamic.php?id=$title.$date'><h2>".$title."</h2></a>";
+                echo "<span class='date'>".$date."</span>";
+                echo "</div>";
+            }
+        }
+    }
+    if ($count == 0) {
+        echo "<p>There is no content yet.</p>";
+    }
+}
+?>
+
+<div class='container'>
+    <nav>
+        <a href='/'>
+            <span lang="EN">Home</span>
+            <span lang="FR">Accueil</span>
+        </a>
+        <a href='/bio/'>
+            <span lang="EN">About</span>
+            <span lang="FR">Biographie</span>
+        </a>
+        <a href='/contact/'>
+            <span lang="EN">Contact Details</span>
+            <span lang="FR">Détails du Contact</span>
+        </a>
+        <a href="/AaronStockdill.pdf" target="_blank">Curriculum Vit&aelig;</a>
+    </nav>
+
+    <!-- <h1 lang="EN">Work</h1> -->
+    <!-- <h1 lang="FR">Ouvrage</h1> -->
+    <ul class='submenu'>
+        <li><a href="#Publications">Publications</a></li>
+        <li><a href="#GitHub">GitHub</a></li>
+        <li><a href="#Writing">Writing</a></li>
+        <li><a href="#Talks">Talks</a></li>
+    </ul>
+
+    <p lang="FR">
+        Ce contenu ne sont pas disponibles en français.
+    </p>
+
+    <h1 id="Publications">Publications</h1>
+<?php
+list_files("pdf");
+?>
+
+    <h1 id="GitHub">GitHub</h1>
+    <p>I infrequently put projects on GitHub, but you are welcome to view what is available there: <a href='https://github.com/aaronstockdill'> Aaron Stockdill on GitHub</a>.</p>
+
+    <h1 id="Writing">Writing</h1>
+<?php
+list_files("md");
+?>
+
+    <h1 id="Talks">Talks</h1>
+    <p>There is no content yet.</p>
+
+</div>
+
+        <footer>
+            <span class='selector'>
+                <a id="white-button" class='theme-button active' onclick='switch_theme("white")'>
+                    <span lang='EN'>light</span>
+                    <span lang='FR'>blanc</span>
+                </a>
+                <a id="black-button" class='theme-button' onclick='switch_theme("black")'>
+                    <span lang='EN'>dark</span>
+                    <span lang='FR'>noir</span>
+                </a>
+            </span>
+            <!--<span class='separator'></span>
+            <span class='selector'>
+                <a id="EN-button" class='lang-button active' onclick='switch_language("EN")'>
+                    EN
+                </a>
+                <a id="FR-button" class='lang-button' onclick='switch_language("FR")'>
+                    FR
+                </a>
+            </span>-->
+        </footer>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.3/jquery.min.js"></script>
+        <script src="/js/preferences.js" charset="utf-8"></script>
+        <?php
+            if (strpos($_SERVER['SERVER_NAME'],
+                       'aaron.stockdill.nz') !== false) {
+        ?>
+        <script>
+            (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+            })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+            ga('create', 'UA-79422930-1', 'auto');
+            ga('send', 'pageview');
+        </script>
+        <?php
+        }
+        ?>
+    </body>
+</html>
